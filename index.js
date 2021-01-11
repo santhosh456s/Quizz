b57d5f3c32f39eec0bf74d8bb8a28f6e5dad464a
santhosh <68135777+santhosh456s@users.noreply.github.com>
2021-01-11 11:15:52 +0530

    basics quizz app factory function in function we can make use of the this function to othr general application

--- index.js
+++ index.js
@@ -1,36 +1,43 @@
-let isCurret = function (choice) {
-  return this.answer.toString() === choice;
-};
 
-let calculateScore = function (choice, timetaken) {
-  if (!isCurret.call(this, choice)) return 0;
+
+function makeQuestion(question,answer,option){
+  return{
+question : question,
+answer : answer,
+option : option,
+isCurret : function (choice) {
+  return this.answer.toString() === choice;
+},
+calculateScore : function (choice, timetaken) {
+  if (!this.isCurret(this, choice)) return 0;
   return (1 / timetaken) * 1000;
-};
+}
+
+  }
+}
 
 let time = 0;
 
 let questions = [
-  {
-    question: "In Java array are",
-    answer: "Object",
-    options: [
+  
+    makeQuestion( "In Java array are",
+     "Object",
+     [
       "Object Refrence",
       "Object",
       "Primitive data type",
       "none of the above"
-    ]
-  },
-  {
-    question:
-      "When you pass the array into the method,the method receives _______",
-    answer: "Copy of the array",
-    options: [
+    ])
+ ,
+  
+    makeQuestion("When you pass the array into the method,the method receives _______","Copy of the array",
+    [
       "refrence of the array",
       "Copy of the array",
       "Length of the array",
       "A copy of the first element"
-    ]
-  }
+    ])
+  
 ];
 
 let currentQuestionIndex = -1;
@@ -42,7 +49,7 @@ let handelClick = function (event) {
     return;
   }
   currentQuestion.answered = true;
-  if (isCurret.call(currentQuestion, event.target.innerHTML)) {
+  if (currentQuestion.isCurret( event.target.innerHTML)) {
     event.target.className = "correct";
     console.log("right");
   } else {
@@ -51,30 +58,30 @@ let handelClick = function (event) {
   }
 
   let currentScore = +document.getElementById("score").innerHTML;
-  let score = calculateScore.call(
-    currentQuestion,
+  let score = currentScore.calculateScore(
+    
     event.target.innerHTML,
     time
   );
 
-  currentScore = score;
- // Number('currentScore');
+  currentScore = parseInt(score);
+  //parseInt('currentScore');
   console.log(currentScore);
   document.getElementById("score").innerHTML = currentScore;
 };
 
 function updatequestion() {
   currentQuestionIndex++;
-  let { question, answer, options, isCurret, calculateScore } = questions[
+  let { question,  option } = questions[
     currentQuestionIndex
   ];
 
   let questionEl = document.getElementById("question");
-  questionEl.innerHTML = question;
+  questionEl.innerHTML =  question;
 
   let optionEl = document.getElementsByTagName("li");
   for (let i = 0; i < optionEl.length; i++) {
-    optionEl[i].innerHTML = options[i];
+    optionEl[i].innerHTML =  option[i];
     optionEl[i].className = " ";
     optionEl[i].addEventListener("click", handelClick);
   }
